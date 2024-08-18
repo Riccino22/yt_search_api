8060739ad50608108cb03e1af93f4611f2c1438e
riccino <riccardorinojosam@gmail.com>
2024-08-18 12:46:46 -0300

    second commit

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import scraping
 
 app = Flask("Api")
 
+
 @app.route("/")
 def home():
     return scraping.get_videos("What is LLM")
 
 app.run(debug=True)
+""
--- scraping.py
+++ scraping.py
@@ -4,26 +4,33 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
- 
+
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
- 
-
 
 def get_videos(title):
     name = title.replace(" ", "+")
     driver.get("https://www.youtube.com/results?search_query={}".format(name))
-
+    
     links_elements = driver.find_elements(By.TAG_NAME, "a")
+    hrefs = [link.get_attribute("href") for link in links_elements if link.get_attribute("href")]
+
     videos = []
-    for link in links_elements:
-        href_text = link.get_attribute("href")
+    videos_titles = []
+    for href_text in list(set(hrefs)):
+        
+        url_format = "https://www.youtube.com/watch?v="
         try:
-            if "https://www.youtube.com/watch?v=" in href_text:
+            if url_format in href_text and href_text not in videos_titles:
+                videos_titles.append(href_text)
                 videos.append(href_text)
+                print(videos_titles)
+                #driver.get(href_text)
+                print(driver.title)
         except TypeError:
             pass
+    
     return videos
